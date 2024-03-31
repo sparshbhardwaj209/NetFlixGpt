c234814ae2ce0ee2c6997fa1508568d40e5c8257
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2024-03-31 17:52:33 +0530

    Css updation

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen px-28 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-full px-28 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={NetflixLogo} alt="Logo" />
       {user && (
         <div className="flex p-2 items-center space-x-2">
